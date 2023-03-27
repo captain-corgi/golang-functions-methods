62b9fcf068ae940b1aeed0435d830275ecba4e62
anh_tuan.l <anh_tuan.l@kbtg.tech>
2023-03-27 14:25:38 +0700

    Add make file and result

---
+++ Makefile
@@ -0,0 +1,6 @@
+run:
+	go run cmd/app/main.go
+
+tidy:
+	go mod tidy
+	go mod vendor
--- cmd/app/main.go
+++ cmd/app/main.go
@@ -3,5 +3,15 @@ package main
 import "fmt"
 
 func main() {
-	fmt.Println("Hello world")
+	fn := GenDisplaceFn(10, 2, 1)
+
+	fmt.Println(fn(3))
+	fmt.Println(fn(5))
+}
+
+// GenDisplaceFn calculate s = ½ a t^2 + vot + so
+func GenDisplaceFn(a, v0, s0 float64) func(t float64) float64 {
+	return func(t float64) float64 {
+		return (a*t*t)/2 + v0*t + s0
+	}
 }
